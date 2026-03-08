9cb8ae1489ebdc4ed60217089244193297d3f457
pengzhuo <pch987.net@163.com>
2026-03-08 18:40:14 +0800

    add id

--- ui/main_window.py
+++ ui/main_window.py
@@ -2,6 +2,7 @@ import sys
 import customtkinter as ctk
 from core.logic import start_process
 
+
 class MainWindow:
 
     def __init__(self):
@@ -11,7 +12,7 @@ class MainWindow:
 
         self.app = ctk.CTk()
         self.app.title("三角洲辅助")
-        self.app.geometry("420x220")
+        self.app.geometry("420x260")
         self.app.resizable(False, False)
 
         self.build_ui()
@@ -28,16 +29,29 @@ class MainWindow:
         frame = ctk.CTkFrame(self.app)
         frame.pack(pady=10)
 
-        label = ctk.CTkLabel(frame, text="子弹价格")
-        label.grid(row=0, column=0, padx=10, pady=10)
+        # 子弹ID
+        label_id = ctk.CTkLabel(frame, text="子弹ID")
+        label_id.grid(row=0, column=0, padx=10, pady=10)
+
+        self.id_input = ctk.CTkEntry(
+            frame,
+            width=180,
+            placeholder_text="输入子弹ID"
+        )
+        self.id_input.grid(row=0, column=1, padx=10)
+
+        # 子弹价格
+        label_price = ctk.CTkLabel(frame, text="子弹价格")
+        label_price.grid(row=1, column=0, padx=10, pady=10)
 
         self.price_input = ctk.CTkEntry(
             frame,
             width=180,
             placeholder_text="输入子弹价格"
         )
-        self.price_input.grid(row=0, column=1, padx=10)
+        self.price_input.grid(row=1, column=1, padx=10)
 
+        # 开始按钮
         start_btn = ctk.CTkButton(
             self.app,
             text="开始",
@@ -52,6 +66,14 @@ class MainWindow:
 
     def start_click(self):
         sys.exit()
+        # bullet_id = self.id_input.get()
+        # price = self.price_input.get()
+
+        # text = start_process(price)
+
+        # self.result.configure(
+        #     text=f"ID:{bullet_id}  价格:{price}"
+        # )
 
     def run(self):
         self.app.mainloop()
