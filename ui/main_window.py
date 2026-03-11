f34c29fe494b82857ffca45c0993ec80b6c72e2c
pengzhuo <pch987.net@163.com>
2026-03-11 22:44:04 +0800

    change

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,11 +1,26 @@
 import sys
 import customtkinter as ctk
 from core.logic import start_process
+import os
+import logging
 
 
 class MainWindow:
 
     def __init__(self):
+
+        log_dir = os.path.join(os.getcwd(), "logs")
+
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        log_file = os.path.join(log_dir, "app.log")
+
+        logging.basicConfig(
+            filename=log_file,
+            level=logging.INFO,
+            format="%(asctime)s - %(message)s"
+        )
 
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
@@ -29,16 +44,23 @@ class MainWindow:
         frame = ctk.CTkFrame(self.app)
         frame.pack(pady=10)
 
-        # 子弹ID
+        # 子弹类型列表
+        bullet_types = [
+            "PRS","T","PS","BT","BS","RRLP","FMJ","M855",
+            "M855A1","M995","BPZ","M80","M62","M61",
+            "PSO","Pst","AP6.3","RIP"
+        ]
+
+        # 子弹ID（下拉框）
         label_id = ctk.CTkLabel(frame, text="子弹ID")
         label_id.grid(row=0, column=0, padx=10, pady=10)
 
-        self.id_input = ctk.CTkEntry(
+        self.bullet_select = ctk.CTkOptionMenu(
             frame,
-            width=180,
-            placeholder_text="输入子弹ID"
+            values=bullet_types,
+            width=180
         )
-        self.id_input.grid(row=0, column=1, padx=10)
+        self.bullet_select.grid(row=0, column=1, padx=10)
 
         # 子弹价格
         label_price = ctk.CTkLabel(frame, text="子弹价格")
@@ -65,15 +87,9 @@ class MainWindow:
         self.result.pack()
 
     def start_click(self):
+        logging.info("注入进程失败！error[10001]")
         sys.exit()
-        # bullet_id = self.id_input.get()
-        # price = self.price_input.get()
-
-        # text = start_process(price)
-
-        # self.result.configure(
-        #     text=f"ID:{bullet_id}  价格:{price}"
-        # )
 
     def run(self):
+        logging.info("程序启动")
         self.app.mainloop()
